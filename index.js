2ebe7a9f7dcefef5896fa4559b694cb96de9a91f
Allie Janoch <allie@mapistry.com>
2017-06-18 21:55:15 -0700

    add a bunch of initial functionality

---
+++ index.js
@@ -0,0 +1,240 @@
+var Date = {
+  startOfDay: function(d) {
+    d.setHours(0);
+    d.setMinutes(0);
+    d.setSeconds(0);
+    d.setMilliseconds(0);
+    return d;
+  },
+
+  endOfDay: function(d) {
+    d.setHours(23);
+    d.setMinutes(59);
+    d.setSeconds(59);
+    d.setMilliseconds(999);
+    return d;
+  },
+
+  getDaysInMonth: function(month, year) {
+    if (month == 8 || month == 3 || month === 5 || month == 10) {
+      return 30;
+    } else if (month === 1) {
+      // February
+      return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0) ? 29 : 28
+    }
+    return 31;
+
+  },
+
+  startOfWeek: function(d) {
+    var dayOfWeek = d.getDay();
+    var dayInMonth = d.getDate();
+
+    if (dayOfWeek >= dayInMonth) {
+      d.setDate(dayInMonth - dayOfWeek);
+      return this.startOfDay(d);
+    }
+    var month = d.getMonth();
+    var year = d.getFullYear();
+    if (month > 0) {
+      d.setMonth(month - 1);
+    } else {
+      d.setMonth(11);
+      d.setFullYear(year - 1);
+    }
+    var daysInMonth = this.getDaysInMonth(d.getMonth(), year);
+    d.setDate(daysInMonth + dayInMonth - dayOfWeek);
+    return this.startOfDay(d);
+
+  },
+
+  endOfWeek: function(d) {
+    var dayOfWeek = d.getDay();
+    var dayInMonth = d.getDate();
+    var year = d.getFullYear();
+    var month = d.getMonth();
+    var daysInMonth = this.getDaysInMonth(d.getMonth(), year);
+
+    if (dayInMonth + 6 - dayOfWeek <= daysInMonth) {
+      d.setDate(dayInMonth + 6 - dayOfWeek);
+      return this.endOfDay(d);
+    }
+
+    if (month < 11) {
+      d.setMonth(month + 1);
+    } else {
+      d.setMonth(0);
+      d.setFullYear(year + 1);
+    }
+    d.setDate(dayInMonth + 6 - dayOfWeek - daysInMonth);
+    return this.endOfDay(d);
+  },
+
+  startOfMonth: function(d) {
+    d.setDate(1);
+    return this.startOfDay(d);
+  },
+
+  endOfMonth: function(d) {
+    d.setDate(this.getDaysInMonth(d.getMonth(), d.getYear()));
+    return this.endOfDay(d);
+  },
+
+  startOfQuarter: function(d) {
+    d.setMonth(Math.floor(d.getMonth()/3) * 3);
+    return this.startOfMonth(d);
+  },
+
+  endOfQuarter: function(d) {
+    d.setMonth(Math.floor(d.getMonth()/3) * 3 + 2);
+    return this.endOfMonth(d);
+  },
+
+  addMonths: function(d, numMonths){
+    var month = d.getMonth();
+    if (month + numMonths < 12) {
+      d.setMonth(month + numMonths);
+      return d;
+    }
+
+    d.setFullYear(d.getFullYear() + 1);
+    d.setMonth(month + numMonths - 12);
+    return d;
+  },
+
+  addLessThan29Days: function(d, daysToAdd) {
+    if (daysToAdd > 28) {
+      throw new Error('Only implemented to add less than one month of days!');
+    }
+
+    var dayOfMonth = d.getDate();
+    var month = d.getMonth();
+    var year = d.getFullYear();
+    var daysInMonth = this.getDaysInMonth(month, year);
+    if (dayOfMonth + daysToAdd <= daysInMonth) {
+      d.setDate(dayOfMonth + daysToAdd);
+      return d;
+    }
+
+    if (month < 11) {
+      d.setMonth(month + 1);
+    } else {
+      d.setMonth(0);
+      d.setFullYear(year + 1);
+    }
+    d.setDate(dayOfMonth + daysToAdd - daysInMonth);
+    return d;
+  },
+
+  subtractLessThan29Days: function(d, daysToSubtract) {
+    if (daysToSubtract > 28) {
+      throw new Error('Only implemented to subtract less than one month of days!');
+    }
+
+    var dayOfMonth = d.getDate();
+    var month = d.getMonth();
+    var year = d.getFullYear();
+    var daysInMonth = this.getDaysInMonth(month, year);
+    if (dayOfMonth - daysToSubtract >= 0) {
+      d.setDate(dayOfMonth - daysToSubtract);
+      return d;
+    }
+
+    if (month > 0) {
+      d.setMonth(month - 1);
+    } else {
+      d.setMonth(11);
+      d.setFullYear(year - 1);
+    }
+    d.setDate(dayOfMonth - daysToSubtract + daysInMonth);
+    return d;
+  },
+
+  isSameDay: function(d1, d2) {
+    return d1.getFullYear() === d2.getFullYear() && d1.getMonth() === d2.getMonth() && d1.getDate() === d2.getDate();
+  },
+
+  isBetween: function(x, d1, d2) {
+    return x.getTime() > d1.getTime() && x.getTime() < d2.getTime();
+  },
+
+  dayOfWeek: function(d, dayOfWeek) {
+    var currentDayOfWeek = d.getDay();
+
+    if (currentDayOfWeek < dayOfWeek) {
+      return this.addLessThan29Days(d, dayOfWeek - currentDayOfWeek);
+    } else if (currentDayOfWeek > dayOfWeek) {
+      return this.subtractLessThan29Days(d, currentDayOfWeek - dayOfWeek);
+    }
+    return d;
+  },
+
+  getMonthName: function(d) {
+    var month = d.getMonth();
+    switch(month) {
+      case 0:
+        return 'January';
+      case 1:
+        return 'February';
+      case 2:
+        return 'March';
+      case 3:
+        return 'April';
+      case 4:
+        return 'May';
+      case 5:
+        return 'June';
+      case 6:
+        return 'July';
+      case 7:
+        return 'August';
+      case 8:
+        return 'September';
+      case 9:
+        return 'October';
+      case 10:
+        return 'November';
+      default:
+        return 'December';
+    }
+  },
+
+  getDayOfYear: function(d){
+    var month = d.getMonth();
+    var i = 0;
+    var dayOfYear = 0;
+    while (i < month) {
+      dayOfYear += this.getDaysInMonth(i++);
+    }
+    return dayOfYear + d.getDate();
+  },
+
+  getWeekOfYear: function(d){
+    var dayOfYear = this.getDayOfYear(d);
+    var weekOfYear = 0;
+    while (weekOfYear*7 < dayOfYear) {
+      weekOfYear++;
+    }
+    return weekOfYear;
+  },
+
+  getShortMonthName: function(d) {
+    this.getMonthName(d).substr(0, 3);
+  },
+
+
+  getQuarter: function(d) {
+    return Math.floor(d.getMonth()/3) + 1;
+  },
+
+  getTwoCharDate: function(d) {
+    var date = d.getDate();
+    if (date < 10) {
+      return ' ' + date;
+    }
+    return date.toString();
+  }
+};
+
+
+module.exports = Date;
---
+++ test.js
@@ -0,0 +1,351 @@
+var expect = require('expect.js');
+var moment = require('moment');
+var MapistryTime = require('./index');
+
+// These tests all confirm functionality by simply testing against moment functions that they are replacing (for speed)
+describe('time library', function() {
+  describe('startOfDay', function() {
+    it('correctly calculates start of day', function() {
+      var m = moment().month(8).date(13).year(2016);
+      var s = MapistryTime.startOfDay(new Date(m.format()));
+      m  = m.startOf('day');
+      expect(s.getDate()).to.be(m.date());
+      expect(s.getMonth()).to.be(m.month());
+      expect(s.getFullYear()).to.be(m.year());
+      expect(s.getHours()).to.be(m.hour());
+      expect(s.getSeconds()).to.be(m.second());
+    });
+  });
+
+  describe('endOfDay', function() {
+    it('correctly calculates end of day', function() {
+      var m = moment().month(8).date(13).year(2016);
+      var s = MapistryTime.endOfDay(new Date(m.format()));
+      m  = m.endOf('day');
+      expect(s.getDate()).to.be(m.date());
+      expect(s.getMonth()).to.be(m.month());
+      expect(s.getFullYear()).to.be(m.year());
+      expect(s.getHours()).to.be(m.hour());
+      expect(s.getSeconds()).to.be(m.second());
+    });
+  });
+
+  describe('startOfWeek', function() {
+    describe ('correctly calculates start of week', function() {
+      it('for day at the beginning of week', function () {
+        var m = moment().month(6).date(9).year(2017);
+        var s = MapistryTime.startOfWeek(new Date(m.format()));
+        m = m.startOf('week');
+        expect(s.getDate()).to.be(m.date());
+        expect(s.getMonth()).to.be(m.month());
+        expect(s.getFullYear()).to.be(m.year());
+        expect(s.getHours()).to.be(m.hour());
+        expect(s.getSeconds()).to.be(m.second());
+      });
+
+      it('for week at the beginning of the month', function () {
+        var m = moment().month(5).date(1).year(2017);
+        var s = MapistryTime.startOfWeek(new Date(m.format()));
+        m = m.startOf('week');
+        expect(s.getDate()).to.be(m.date());
+        expect(s.getMonth()).to.be(m.month());
+        expect(s.getFullYear()).to.be(m.year());
+        expect(s.getHours()).to.be(m.hour());
+        expect(s.getSeconds()).to.be(m.second());
+      });
+
+      it('for week at the beginning of the year', function () {
+        var m = moment().month(0).date(1).year(2016);
+        var s = MapistryTime.startOfWeek(new Date(m.format()));
+        m = m.startOf('week');
+        expect(s.getDate()).to.be(m.date());
+        expect(s.getMonth()).to.be(m.month());
+        expect(s.getFullYear()).to.be(m.year());
+        expect(s.getHours()).to.be(m.hour());
+        expect(s.getSeconds()).to.be(m.second());
+      });
+    });
+  });
+
+  describe('endOfWeek', function() {
+    describe ('correctly calculates end of week', function() {
+      it('for day at the end of week', function () {
+        var m = moment().month(6).date(15).year(2017);
+        var s = MapistryTime.endOfWeek(new Date(m.format()));
+        m = m.endOf('week');
+        expect(s.getDate()).to.be(m.date());
+        expect(s.getMonth()).to.be(m.month());
+        expect(s.getFullYear()).to.be(m.year());
+        expect(s.getHours()).to.be(m.hour());
+        expect(s.getSeconds()).to.be(m.second());
+      });
+
+      it('for week at the end of the month', function () {
+        var m = moment().month(5).date(29).year(2017);
+        var s = MapistryTime.endOfWeek(new Date(m.format()));
+        m = m.endOf('week');
+        expect(s.getDate()).to.be(m.date());
+        expect(s.getMonth()).to.be(m.month());
+        expect(s.getFullYear()).to.be(m.year());
+        expect(s.getHours()).to.be(m.hour());
+        expect(s.getSeconds()).to.be(m.second());
+      });
+
+      it('for week at the end of the year', function () {
+        var m = moment().month(11).date(12).year(2017);
+        var s = MapistryTime.endOfWeek(new Date(m.format()));
+        m = m.endOf('week');
+        expect(s.getDate()).to.be(m.date());
+        expect(s.getMonth()).to.be(m.month());
+        expect(s.getFullYear()).to.be(m.year());
+        expect(s.getHours()).to.be(m.hour());
+        expect(s.getSeconds()).to.be(m.second());
+      });
+    });
+  });
+
+  describe('startOfMonth', function() {
+    it('correctly calculates start of month', function() {
+      var m = moment().month(6).date(15).year(2017);
+      var s = MapistryTime.startOfMonth(new Date(m.format()));
+      m = m.startOf('month');
+      expect(s.getDate()).to.be(m.date());
+      expect(s.getMonth()).to.be(m.month());
+      expect(s.getFullYear()).to.be(m.year());
+      expect(s.getHours()).to.be(m.hour());
+      expect(s.getSeconds()).to.be(m.second());
+    });
+  });
+
+  describe('endOfMonth', function() {
+    it('correctly calculates end of month', function() {
+      var m = moment().month(6).date(15).year(2017);
+      var s = MapistryTime.endOfMonth(new Date(m.format()));
+      m = m.endOf('month');
+      expect(s.getDate()).to.be(m.date());
+      expect(s.getMonth()).to.be(m.month());
+      expect(s.getFullYear()).to.be(m.year());
+      expect(s.getHours()).to.be(m.hour());
+      expect(s.getSeconds()).to.be(m.second());
+    });
+  });
+
+  describe('startOfQuarter', function() {
+    it('correctly calculates start of quarter', function() {
+      var m = moment().month(6).date(15).year(2017);
+      var s = MapistryTime.startOfQuarter(new Date(m.format()));
+      expect(s.getDate()).to.be(1);
+      expect(s.getMonth()).to.be(6);
+      expect(s.getFullYear()).to.be(2017);
+      expect(s.getHours()).to.be(0);
+      expect(s.getSeconds()).to.be(0);
+    });
+  });
+
+  describe('endOfQuarter', function() {
+    it('correctly calculates end of quarter', function() {
+      var m = moment().month(6).date(15).year(2017);
+      var s = MapistryTime.endOfQuarter(new Date(m.format()));
+      expect(s.getDate()).to.be(30);
+      expect(s.getMonth()).to.be(8);
+      expect(s.getFullYear()).to.be(2017);
+      expect(s.getHours()).to.be(23);
+      expect(s.getSeconds()).to.be(59);
+    });
+  });
+
+  describe('getDaysInMonth', function() {
+    describe('gets correct days for February', function() {
+      it ('in a leap year', function() {
+        expect(MapistryTime.getDaysInMonth(1, 2016), 29);
+      });
+
+      it ('in a non leap year', function() {
+        expect(MapistryTime.getDaysInMonth(1, 2015), 28);
+      });
+    });
+
+    it('gets correct days for June', function() {
+      expect(MapistryTime.getDaysInMonth(5, 2015), 30);
+    });
+
+    it('gets correct days for January', function() {
+      expect(MapistryTime.getDaysInMonth(0, 2015), 31);
+    });
+  });
+
+  describe('addMonths', function() {
+    it('add months so that year does not change', function() {
+      var m = moment().month(6).date(5).year(2017);
+      var s = MapistryTime.addMonths(new Date(m.format()), 2);
+      m = m.add(2, 'months');
+      expect(s.getDate()).to.be(m.date());
+      expect(s.getMonth()).to.be(m.month());
+      expect(s.getFullYear()).to.be(m.year());
+      expect(s.getHours()).to.be(m.hour());
+      expect(s.getSeconds()).to.be(m.second());
+    });
+
+    it('add months so that year does change', function() {
+      var m = moment().month(12).date(5).year(2017);
+      var s = MapistryTime.addMonths(new Date(m.format()), 2);
+      m = m.add(2, 'months');
+      expect(s.getDate()).to.be(m.date());
+      expect(s.getMonth()).to.be(m.month());
+      expect(s.getFullYear()).to.be(m.year());
+      expect(s.getHours()).to.be(m.hour());
+      expect(s.getSeconds()).to.be(m.second());
+    });
+  });
+
+  describe('addLessThan29Days', function() {
+    it('add two weeks so that year does not change', function() {
+      var m = moment().month(6).date(5).year(2017);
+      var s = MapistryTime.addLessThan29Days(new Date(m.format()), 14);
+      m = m.add(2, 'weeks');
+      expect(s.getDate()).to.be(m.date());
+      expect(s.getMonth()).to.be(m.month());
+      expect(s.getFullYear()).to.be(m.year());
+      expect(s.getHours()).to.be(m.hour());
+      expect(s.getSeconds()).to.be(m.second());
+    });
+
+    it('add two weeks so that year does change', function() {
+      var m = moment().month(12).date(25).year(2017);
+      var s = MapistryTime.addLessThan29Days(new Date(m.format()), 14);
+      m = m.add(2, 'weeks');
+      expect(s.getDate()).to.be(m.date());
+      expect(s.getMonth()).to.be(m.month());
+      expect(s.getFullYear()).to.be(m.year());
+      expect(s.getHours()).to.be(m.hour());
+      expect(s.getSeconds()).to.be(m.second());
+    });
+  });
+
+  describe('subtractLessThan29Days', function() {
+
+    it('subtract days so that month and year do not change', function() {
+      var m = moment().month(6).date(5).year(2017);
+      var s = MapistryTime.subtractLessThan29Days(new Date(m.format()),2);
+      m = m.subtract(2, 'days');
+      expect(s.getDate()).to.be(m.date());
+      expect(s.getMonth()).to.be(m.month());
+      expect(s.getFullYear()).to.be(m.year());
+      expect(s.getHours()).to.be(m.hour());
+      expect(s.getSeconds()).to.be(m.second());
+    });
+
+    it('subtract days so that month and year do change', function() {
+      var m = moment().month(0).date(5).year(2017);
+      var s = MapistryTime.subtractLessThan29Days(new Date(m.format()),7);
+      m = m.subtract(7, 'days');
+      expect(s.getDate()).to.be(m.date());
+      expect(s.getMonth()).to.be(m.month());
+      expect(s.getFullYear()).to.be(m.year());
+      expect(s.getHours()).to.be(m.hour());
+      expect(s.getSeconds()).to.be(m.second());
+    });
+  });
+
+  describe('isSameDay', function() {
+    var m2, d2;
+    beforeEach(function() {
+      m2 = moment().startOf('day').add(10, 'hours');
+      d2 = new Date(m2.format());
+    });
+
+    it('returns true if same day', function() {
+      var m1 = moment().startOf('day').add(4, 'hours');
+      var d1 = new Date(m1.format());
+      expect(m1.isSame(m2, 'day')).to.be.ok();
+      expect(MapistryTime.isSameDay(d1, d2)).to.be.ok();
+    });
+
+    it('otherwise returns false', function() {
+      var m1 = moment().startOf('day').subtract(4, 'hours');
+      var d1 = new Date(m1.format());
+      expect(m1.isSame(m2, 'day')).to.not.be.ok();
+      expect(MapistryTime.isSameDay(d1, d2)).to.not.be.ok();
+    });
+  });
+
+  describe('isBetween', function() {
+    var m1, m2, d1, d2;
+    beforeEach(function() {
+      m1 = moment().startOf('day').add(4, 'hours');
+      m2 = moment().startOf('day').add(10, 'hours');
+      d1 = new Date(m1.format());
+      d2 = new Date(m2.format());
+    });
+
+    it('returns true if date is between two dates', function() {
+      var mb = moment().startOf('day').add(6, 'hours');
+      var b = new Date(mb.format());
+      expect(mb.isBetween(m1, m2)).to.be.ok();
+      expect(MapistryTime.isBetween(b, d1, d2)).to.be.ok();
+    });
+
+    it('returns false if date is not between two dates', function() {
+      var mb = moment().startOf('day').add(2, 'days');
+      var b = new Date(mb.format());
+      expect(mb.isBetween(m1, m2)).to.not.be.ok();
+      expect(MapistryTime.isBetween(b, d1, d2)).to.not.be.ok();
+    });
+  });
+
+  describe('dayOfWeek', function() {
+    it('changes day of week to Monday', function() {
+      var m = moment().month(5).date(21).year(2017);
+      var s = MapistryTime.dayOfWeek(new Date(m.format()), 1);
+      m = m.isoWeekday(1);
+      expect(s.getDate()).to.be(m.date());
+      expect(s.getMonth()).to.be(m.month());
+      expect(s.getFullYear()).to.be(m.year());
+      expect(s.getHours()).to.be(m.hour());
+      expect(s.getSeconds()).to.be(m.second());
+    });
+
+    it('changes day of week to Friday', function() {
+      var m = moment().month(5).date(21).year(2017);
+      var s = MapistryTime.dayOfWeek(new Date(m.format()), 5);
+      m = m.isoWeekday(5);
+      expect(s.getDate()).to.be(m.date());
+      expect(s.getMonth()).to.be(m.month());
+      expect(s.getFullYear()).to.be(m.year());
+      expect(s.getHours()).to.be(m.hour());
+      expect(s.getSeconds()).to.be(m.second());
+    });
+  });
+
+  describe('getQuarter', function() {
+    it ('returns same quarter as moment would', function() {
+      var m = moment().month(5).date(21).year(2017);
+      var d = new Date(m.format());
+      expect(MapistryTime.getQuarter(d)).to.be(m.quarter());
+    });
+  });
+
+  describe('getDayOfYear', function() {
+    it ('returns same day as moment would', function() {
+      var m = moment().month(5).date(21).year(2017);
+      var d = new Date(m.format());
+      expect(MapistryTime.getDayOfYear(d)).to.be(parseInt(m.format('DDD')));
+    });
+  });
+
+  describe('getWeekOfYear', function() {
+    it ('returns same week as moment would', function() {
+      var m = moment().month(5).date(21).year(2017);
+      var d = new Date(m.format());
+      expect(MapistryTime.getWeekOfYear(d)).to.be(parseInt(m.format('w')));
+    });
+  });
+
+  describe('getTwoCharDate', function() {
+    it ('returns same string as moment would', function() {
+      var m = moment().month(5).date(21).year(2017);
+      var d = new Date(m.format());
+      expect(MapistryTime.getTwoCharDate(d)).to.be(m.format('DD'));
+    });
+  })
+});
